0fc730ebd2ec78be861b813299923c3788206c20
shivamt0602 <106347133+shivamt0602@users.noreply.github.com>
2023-10-19 20:20:55 +0530

    use of previous value in useState hook.

---
+++ App.js
@@ -0,0 +1,80 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About'; 
+import { useState } from 'react';
+
+// import {
+//   BrowserRouter as Router,
+//   Routes, // we used Routes instead of switches
+//   Route
+// } from "react-router-dom"; 
+
+
+
+
+function App() { 
+
+  const[fullname,Setfullname] = useState(
+
+    {
+      fname : "",
+      lname : ""
+    } 
+
+  )
+
+  function handlechange(event){
+
+    let name = event.target.name;
+    let new_val = event.target.value;
+    console.log(name);
+    console.log(new_val);
+
+    Setfullname((prevValue)=>{ // function needed as for objects key value is deleted so there is no way that we can get the previous values again.
+      if(name === 'first-name'){
+        return{
+          fname : new_val,
+          lname : prevValue.lname
+        }
+      }
+      else{
+        return{
+          fname : prevValue.fname,
+          lname : new_val
+        }
+      }
+    })
+    
+
+  }
+
+
+ 
+
+  return (
+    <>
+    {/* <Router>
+    <div className='Container'>
+      <Routes>
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/" element={<Navbar current_action = "asap"/>} />
+      </Routes>
+    </div>
+    </Router> */}
+    {/* <Navbar/>
+    <About/> */}
+    <form>
+      <h1>Hello {fullname.fname} {fullname.lname}</h1>
+      <div className='input-class'>
+        <input placeholder='first name' type='text' onChange={handlechange} name = "first-name" value = {fullname.fname}/>
+        <input placeholder='last-name' type='text' onChange={handlechange} name="last-name" value = {fullname.lname}/>
+        <button className='btn-1'>Submit</button>
+      </div>
+    </form>
+    </>
+    
+  );
+}
+
+export default App;
